0bd035f48049c17d84eedf01790fef9688014eec
syemie069 <syemie069@gmail.com>
2026-03-08 23:44:08 +0530

    build error

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,5 +1,5 @@
 
-const Product = require('../models/product');
+const Product = require('../models/Product');
 const Category = require('../models/Category');
 
 exports.createProduct = async (req, res, next) => {
