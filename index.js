e88952490e73ded3101ba042ff301e24c3da2290
syemie069 <syemie069@gmail.com>
2026-03-09 15:55:30 +0530

    fix

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(clientBuildPath, 'index.html'));
     });
 }
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
